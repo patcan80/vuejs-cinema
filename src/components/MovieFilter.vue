8ad2ebeccd5e093d1d9ee923389d203836ceccae
Patrick Candler <patcan_80@yahoo.com>
2018-11-25 23:31:55 -0500

    add components

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="movie-filter">
+        <h2>Filter results</h2>
+        <div class="filter-group">
+            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+        </div>
+    </div>
+</template>
+
+<script>
+    import CheckFilter from './CheckFilter.vue';
+    import genres from '../util/genres';
+
+    export default {
+        name: "MovieFilter",
+        data() {
+            return {
+                genres
+            };
+        },
+        methods: {
+            checkFilter(category, title, checked) {
+                this.$emit('check-filter', category, title, checked);
+            }
+        },
+        components: {
+            CheckFilter
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="movie-list">
+        <movie-item v-for="movie in filteredMovies" v-bind:movie="movie.movie"></movie-item>
+    </div>
+</template>
+
+<script>
+    import MovieItem from './MovieItem.vue'
+    export default {
+        name: "MovieList",
+        props: [ 'genre', 'time', 'movies' ],
+        methods: {
+            moviePassesGenreFilter(movie){
+                if (!this.genre.length){
+                    return true;
+                } else {
+                    return this.genre.find(genre => movie.genre === genre);
+                }
+
+            }
+        },
+        computed: {
+            filteredMovies() {
+                return this.movies.filter(this.moviePassesGenreFilter);
+            }
+        },
+        components: {
+            MovieItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
